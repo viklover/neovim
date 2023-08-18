13e1c76647187c53f86a9e148eac3af7d01e3798
Mihail Vorobyev <1viklover@gmail.com>
2023-08-18 15:32:25 +0500

    [INIT] initial commit

---
+++ lua/core/plugins.lua
@@ -0,0 +1,21 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+        lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { 'phaazon/hop.nvim' },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim", "s1n7ax/nvim-window-picker"
+        }
+    }
+})
